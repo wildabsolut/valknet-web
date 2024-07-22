ffa184fad3df0d105d8a7f2eaea3c0f35d728b85
absolut <120755602+wildabsolut@users.noreply.github.com>
2024-07-22 11:41:11 -0700

    a

--- cdn/security.js
+++ cdn/security.js
@@ -1,2 +1,3 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4318108302728136"crossorigin="anonymous"></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4318108302728136"
+     crossorigin="anonymous"></script>
 // im testing something vzlol
