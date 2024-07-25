cf12f7175a6e0575208ef81698cfbe9aeb62c5e5
absolut <120755602+wildabsolut@users.noreply.github.com>
2024-07-24 17:55:20 -0700

    Fixed bugs.

--- cdn/createaccount.js
+++ cdn/createaccount.js
@@ -16,10 +16,34 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 
-const signup = document.getElementById('signup');
-if (signup) {
-    const submitbutton = document.getElementById('signup-button');
-    signup.addEventListener('click', () => {
+const submitbutton = document.getElementById('signup-button');
+if (submitbutton) {
+    submitbutton.addEventListener('click', () => {
+        const username = document.getElementById("username").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        if (username) {
+
+        } else {
+            document.getElementById("message").innerHTML = "<p class='error-message'>Please fill in your username.</p>";
+            return;
+        }
+
+        if (email) {
+
+        } else {
+            document.getElementById("message").innerHTML = "<p class='error-message'>Please fill in your email address.</p>";
+            return;
+        }
+
+        if (password) {
+
+        } else {
+            document.getElementById("message").innerHTML = "<p class='error-message'>Please fill in your password.</p>";
+            return;
+        }
+
         document.getElementById("message").innerHTML = "<p class='error-message'>Your account has been suspended. Please E-mail contact@valknet.xyz for more information.</p>";
         submitbutton.innerHTML = "<div class='loader'></div>";
         document.getElementById("username").disabled = true;
