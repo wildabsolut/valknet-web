8cfb94abad2a51793ed3333b9d7ef6c7462dc7d1
absolut <120755602+wildabsolut@users.noreply.github.com>
2024-07-25 11:45:32 -0700

    Added Google Analytics.

---
+++ cdn/loader.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Z0C46KN3YC"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Z0C46KN3YC');
+</script>
